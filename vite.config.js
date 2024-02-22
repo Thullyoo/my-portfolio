cf5915844f886afa37b8160c564854958dd82d9a
Thullyoo <123775158+Thullyoo@users.noreply.github.com>
2024-02-22 11:17:58 -0300

    feat: config vite

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/my-pessoal-portfolio",
-  root: "../my-portfolio"
+  root: "../my-pessoal-portfolio"
 });
