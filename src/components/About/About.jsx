81af0685a2acab8ed9868665610c69dd3ba4fbb3
Thullyoo <thullyofilho@gmail.com>
2024-04-15 13:02:41 -0300

    feat: ajustes no about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,5 @@
 import styles from './About.module.css'
-import { FaReact } from "react-icons/fa";
+import { FaPython, FaReact } from "react-icons/fa";
 import { FaJava } from "react-icons/fa";
 import { BiLogoSpringBoot } from "react-icons/bi";
 import { FaAngular } from "react-icons/fa";
@@ -9,15 +9,16 @@ const About =() => {
  return(
   <div className={styles.about_container}>
    <div className={styles.about_text}>
-    <h1>Olá me chamo Thúllyo e sou um desenvolvedor Full-Stack JAVA</h1>
-    <h2>Atualmente estou matriculado na UNESA em Sistemas de Informação no 3 período, e venho estudando para me tornar um desenvolvedor Full-Stack ou Back-End JAVA, minhas principais tecnologias são:</h2>
+    <h1>Olá me chamo Thúllyo e sou um desenvolvedor Back-end JAVA</h1>
+    <h2>Atualmente estou matriculado na UNESA em Sistemas de Informação no 3 período, e venho estudando para me tornar um desenvolvedor Back-end JAVA ou Full-Stack, minhas principais tecnologias são:</h2>
    </div>
    <div className={styles.about_tec}>
    <ul>
-      <li><FaReact className={styles.react_icon}/></li>
+      
       <li><FaJava className={styles.java_icon}></FaJava></li>
       <li><BiLogoSpringBoot className={styles.spring_icon}></BiLogoSpringBoot></li>
-      <li><FaAngular className={styles.angular_icon}></FaAngular></li>
+      <li><FaPython className={styles.python_icon}></FaPython></li>
+      <li><FaReact className={styles.react_icon}/></li>
      </ul>
    </div>
   </div>
