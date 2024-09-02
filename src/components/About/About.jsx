7fe6fb186a71f69b46c3fc5ecd054d931bf31df2
Thullyoo <thullyofilho@gmail.com>
2024-09-02 15:50:42 -0300

    feat: change about and css fix

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,7 +8,7 @@ const About =() => {
   <div className={styles.about_container}>
    <div className={styles.about_text}>
     <h1>Olá me chamo Thúllyo e sou um desenvolvedor Back-end JAVA</h1>
-    <h2>Atualmente estou matriculado na UNESA em Sistemas de Informação no 3 período, e venho estudando para me tornar um desenvolvedor Back-end JAVA ou Full-Stack, minhas principais tecnologias são:</h2>
+    <h2>Atualmente estou matriculado na UNESA em Sistemas de Informação no 4 período, e venho estudando para me tornar um desenvolvedor Back-end JAVA ou Full-Stack, minhas principais tecnologias são:</h2>
    </div>
    <div className={styles.about_tec}>
    <ul>
